02515eb1756796a1aba8dd2aef95af5584a4c2ea
nkj5123 <nkj5123@psu.edu>
2022-04-16 22:33:39 -0400

    Changed

---
+++ src/Viewer.js
@@ -0,0 +1,16 @@
+// ./src/components/Viewer.js
+
+// Imports
+import React from "react";
+
+// Viewer segment container
+export default function Viewer({ svgData }) {
+    // Contains the SVG display structure for the application
+
+    // Returns a JSX component for the SVG display
+    return (
+        <>
+            <h1>This is the viewer!</h1>
+        </>
+    );
+}
